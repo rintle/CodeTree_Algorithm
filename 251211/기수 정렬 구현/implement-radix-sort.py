fd166fa343de6edc4ff66357915cab36c9f3b0e7
Seunghwan Kang <rnltls97@gmail.com>
2025-12-11 11:25:05 +0900

    Title: 기수 정렬 구현, Time: 90ms, Memory: 21MB, Status: Wrong Answer - Codetree

--- 251211/기수 정렬 구현/implement-radix-sort.py
+++ 251211/기수 정렬 구현/implement-radix-sort.py
@@ -5,14 +5,14 @@ for pos in range(5, -1, -1):
     arr_new = [[] for _ in range(10)]
     for i in range(n):
         div_val = 10**pos
-        digit = nums[i] // div_val
+        digit = (nums[i] // div_val)%10
         arr_new[digit].append(nums[i])
     store_arr = []
     for i in range(10):
         for num in arr_new[i]:
             store_arr.append(num)
     nums = store_arr
-    
+
 for i in store_arr:
     print(i, end=' ')
         
