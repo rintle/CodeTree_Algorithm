3c42bcf899c163c7ff072daa32fcdfa5d2e5c721
Seunghwan Kang <rnltls97@gmail.com>
2025-12-12 14:30:08 +0900

    Title: 퀵 정렬 구현, Time: 160ms, Memory: 25MB, Status: Passed - Codetree

--- 251212/퀵 정렬 구현/implement-quick-sort.py
+++ 251212/퀵 정렬 구현/implement-quick-sort.py
@@ -11,8 +11,9 @@ def partition(arr, low, high):
         if arr[j] < pivot:
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
-    arr[i+1], arr[high] = arr[high], arr[i+1]
-    return i+1
+    i += 1
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
 
 def quicksort(arr, low, high):
     if low < high:
